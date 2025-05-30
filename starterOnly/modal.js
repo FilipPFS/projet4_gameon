c53cfc7fd65de46388257e196d566b9ddd18501b
Filip Petrovic <filippetrovicfr04@gmail.com>
2025-05-30 12:23:19 +0200

    Création de la fonctionnalité pour fermer notre modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,12 +10,21 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("formElement");
+const modalBody = document.querySelector(".modal-body");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+  form.reset();
+  modalbg.style.display = "none";
 }
